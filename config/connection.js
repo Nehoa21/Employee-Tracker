6547b8f7a73fbec86bac03877b0ef2479bd12125
Nehoa21 <nehoaakina21@yahoo.com>
2023-11-13 16:48:15 -0700

    updated and created files and folders for better structure

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+const Sequalize = require('sequalize');
+require('dotenv').config();
+
+let sequalize;
+
+sequalize = new Sequalize (
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    }
+);
+
+module.exports = sequalize;
