f44aee48598900af292a5976640ae01dde1f2875
Nehoa21 <nehoaakina21@yahoo.com>
2023-11-13 17:08:35 -0700

    edited schema.sql, deleted CREATE DATABASES

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,4 @@
-DROP DATABASE IF EXISTS company;
-CREATE DATABASE company;
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
 
-USE company;
-
-CREATE TABLE department(
-    dep_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    dep_name VARCHAR(30) NOT NULL
-);
-
-CREATE TABLE role(
-    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT,
-    FOREIGN KEY (department_id)
-    REFERENCES department(dep_id)
-    ON DELETE SET NULL
-);
-
-CREATE TABLE employee(
-    employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_naem VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    manager_id INT,
-    FOREIGN KEY (role_id), (manager_id)
-    REFERENCES role(id), employee(id)
-    ON DELETE SET NULL
-);
+USE company_db;
